9a8c505a537b2ad650518fa3ca497e960f1ed83e
yomZsamora <samora.y@adzumi.co.ke>
2022-01-20 16:43:32 +0300

    returns an array of thank you messages for each name provided to the function.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+// Returns an array of thank you messages for each name provided to the function
+let writeCards = (listOfNames, nameOfEvent) => {
+   let thankYouMessages = [];
+   for(let i = 0; i < listOfNames.length; i++) {
+      thankYouMessages.push(`Thank you, ${listOfNames[i]}, for the wonderful ${nameOfEvent} gift!`);
+   }
+   return thankYouMessages;
+}
